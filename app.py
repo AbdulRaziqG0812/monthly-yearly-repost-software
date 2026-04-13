dda1ee2641df23189074c91556b8637e765bff68
Abdul Raziq <abdul.raziq12003G@gmail.com>
2026-04-13 21:55:06 +0500

    comit this

--- app.py
+++ app.py
@@ -4015,4 +4015,4 @@ def edit_summarisereport_list(id):
     return render_template('edit_summarisereport_list.html', record=record)
 
 if __name__ == '__main__':
-    app.run(host="172.16.48.56", port=5500, debug=True)
+    app.run(port=5600, debug=True)
--- flask_start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd "/Users/apple/Downloads/NHAmon/yearreport"
-python3 app.py
